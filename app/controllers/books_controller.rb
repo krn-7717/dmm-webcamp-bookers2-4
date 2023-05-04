3f9302ebf4e5f84477ad5740d8608ca2c673e9db
EC2 Default User <ec2-user@ip-172-31-39-166.ap-northeast-1.compute.internal>
2023-05-04 19:25:47 +0000

    [Finish]4-25

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @user = current_user
+
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      flash[:notice] = "1 error prohibited this obj from being saved:"
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+    @book_dil = Book.find(params[:id])
+    @user = @book_dil.user
+    @book = Book.new()
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+
+    if @book.user != current_user
+      redirect_to books_path
+      return
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      flash[:notice] = "error"
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  # get 'users/show'
+  # get 'users/edit'
+  resources :users, only: [:show, :edit, :index, :update]
+
+  # get 'books/new'
+  # get 'books/show'
+  # get 'books/index'
+  # get 'books/edit'
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+
+  # get 'homes/top'
+  root to: "homes#top"
+  get 'home/about' => 'homes#about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
